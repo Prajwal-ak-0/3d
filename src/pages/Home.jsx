80ecf49b274a7c4ed869cd468b30911e71b927c0
Prajwal <prajwalhassan7@gmail.com>
2023-11-26 11:36:42 +0530

    Able to see Island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
+/* eslint-disable no-unused-vars */
 import { Canvas } from "@react-three/fiber"
 import { Suspense } from "react"
 import Loader from "../components/Loader"
 import { Island } from "../models/Island"
 
 const Home = () => {
+    const adjustIslandForScreenSize = () => {
+        let screenScale = null;
+        let screenPosition = [0, -6.5, -43];
+        let rotation = [0.1,4.7,0];
+
+        if (window.innerWidth < 640) {
+            screenScale = [0.9, 0.9, 0.9];
+        } else{
+            screenScale = [1,1,1];
+        }
+
+        return [screenScale, screenPosition, rotation];
+    } 
+
+    const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
         {/* 
@@ -21,7 +38,11 @@ const Home = () => {
                 <pointLight/>
                 <spotLight/>
                 <hemisphereLight/>
-                <Island/>
+                <Island
+                    scale={islandScale}
+                    position={islandPosition}
+                    rotation={islandRotation}
+                />
             </Suspense>
         </Canvas>
     </section>
